76c664fe9e3ed5cce014629720a5fc2bbc2bda8d
Jeremy Grecu <jgrecu@gmail.com>
2025-04-25 15:42:18 +0200

    stage 10: handle multiple encodings.

--- app/main.go
+++ app/main.go
@@ -84,40 +84,44 @@ func main() {
 				}
 				content := uriParts[2]
 
-				if encoding, ok := request.Headers["Accept-Encoding"]; ok {
-					if encoding == "gzip" {
-						var b bytes.Buffer
-						gz := gzip.NewWriter(&b)
-						if _, err := gz.Write([]byte(content)); err != nil {
-							log.Fatal(err)
-						}
+				if encodingStr, ok := request.Headers["Accept-Encoding"]; ok {
+					encodings := strings.Split(encodingStr, ", ")
+					for _, encoding := range encodings {
+						if encoding == "gzip" {
+							var b bytes.Buffer
+							gz := gzip.NewWriter(&b)
+							if _, err := gz.Write([]byte(content)); err != nil {
+								log.Fatal(err)
+							}
 
-						contentLength := len(b.Bytes())
-						conn.Write([]byte(fmt.Sprintf(
-							"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\nContent-Encoding: %s\r\n\r\n%s",
-							contentLength,
-							encoding,
-							b.Bytes(),
-						)))
+							contentLength := len(b.Bytes())
+							conn.Write(fmt.Appendf(nil,
+								"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\nContent-Encoding: %s\r\n\r\n%s",
+								contentLength,
+								encoding,
+								b.Bytes(),
+							))
+							return
+						}
 					}
 				}
 
 				contentLength := utf8.RuneCountInString(content)
 
-				conn.Write([]byte(fmt.Sprintf(
+				conn.Write(fmt.Appendf(nil,
 					"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",
 					contentLength,
 					content,
-				)))
+				))
 			} else if strings.HasPrefix(request.Url, "/user-agent") {
 				content := request.Headers["User-Agent"]
 				contentLength := utf8.RuneCountInString((content))
 
-				conn.Write([]byte(fmt.Sprintf(
+				conn.Write(fmt.Appendf(nil,
 					"HTTP/1.1 200 OK\r\nContent-Type: text/plain\r\nContent-Length: %d\r\n\r\n%s",
 					contentLength,
 					content,
-				)))
+				))
 			} else if strings.HasPrefix(request.Url, "/files") {
 				uriParts := strings.Split(request.Url, "/")
 				if len(uriParts) > 3 {
@@ -134,10 +138,10 @@ func main() {
 
 					content, _ := os.ReadFile(fmt.Sprintf("/%s/%s", tempDirectory, path))
 					contentLength := utf8.RuneCountInString(string(content))
-					conn.Write([]byte(fmt.Sprintf(
+					conn.Write(fmt.Appendf(nil,
 						"HTTP/1.1 200 OK\r\nContent-Type: application/octet-stream\r\nContent-Length: %d\r\n\r\n%s",
 						contentLength,
-						content)))
+						content))
 				} else if request.Method == "POST" {
 					os.WriteFile(fmt.Sprintf("/%s/%s", tempDirectory, path), request.Body, 0666)
 					conn.Write([]byte("HTTP/1.1 201 Created\r\n\r\n"))
